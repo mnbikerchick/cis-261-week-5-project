4a74cb159fb47866d8825009249e4a759ac8ece0
Jeanne Olsen <Jeanne.Olsen@my.strayer.edu>
2022-11-05 16:39:45 -0500

    much better except grosspay not defined

--- cis_261_week_5_project.py
+++ cis_261_week_5_project.py
@@ -31,14 +31,13 @@ def printinfo(EmpDetailList):
 # the following code creates a for loop to read through EmpDetailList and assign values in list to variables
     for EmpList in EmpDetailList:
         fromdate = EmpList[0]
-        todate = EmpList[0]
-        empname = EmpList[0]
-        hours = EmpList[0]
-        hourlyrate = EmpList[0]
-        taxrate = EmpList[0]
+        todate = EmpList[1]
+        empname = EmpList[2]
+        hours = EmpList[3]
+        hourlyrate = EmpList[4]
+        taxrate = EmpList[5]
         grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
         print(fromdate, todate, empname, f"{hours:,.2f}",  f"{hourlyrate:,.2f}", f"{grosspay:,.2f}",  f"{taxrate:,.1%}",  f"{incometax:,.2f}",  f"{netpay:,.2f}")
-        
         TotEmployees += 1
         TotHours += hours
         TotGrossPay += grosspay
@@ -59,14 +58,13 @@ def PrintTotals(EmpTotals):
     print(f'Total Net Pay: {EmpTotals["TotNetPay"]}')
 
 if __name__ == "__main__":
-    # COMMENT OUT THE FOLLOWING CODE
-    # #TotEmployees = 0
-    #TotHours = 0.00
-    #TotGrossPay = 0.00
-    #TotTax = 0.00
-    #TotNetPay = 0.00
+    
+    TotEmployees = 0
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
 
-    #create empty list and dictionary
     EmpDetailList = []
     EmpTotals = {}
     while True:
@@ -78,19 +76,12 @@ if __name__ == "__main__":
         hourlyrate = GetHourlyRate()
         taxrate = GetTaxRate()
 EmpDetail = (fromdate, todate, empname, hours, hourlyrate, taxrate)
-   #write code to insert fromdate, todate, empname, hours, hourlyrate, and taxrate into list EmpDetail
-   #the following code appends the list EmpDetail to the list EmpDetailList
 EmpDetailList.append(EmpDetail)
 
-        # COMMENT OUT THE FOLLOWING CODE
-        #TotEmployees += 1
-        #TotHours += hours
-        #TotGrossPay += grosspay
-        #TotTax += incometax
-        #TotNetPay += netpay
+TotEmployees += 1
+TotHours += hours
+TotGrossPay += grosspay
+TotTax += incometax
+TotNetPay += netpay
 printinfo (EmpDetailList)
 PrintTotals (EmpTotals)
-
-
-
-
